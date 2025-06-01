02a34f430af6c54435a36f01716a159965b5c1e2
João Pedro De Araújo Lemos <144943620+joaopedrolemos6@users.noreply.github.com>
2025-05-31 21:05:30 -0300

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -4,7 +4,7 @@ import { MessageCircle } from 'lucide-react';
 const WhatsAppButton: React.FC = () => {
   return (
     <a
-      href="https://wa.me/5583999245858" // Replace with your actual WhatsApp number
+      href="https://wa.me/5583996768141" // Replace with your actual WhatsApp number
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white rounded-full p-3 shadow-lg hover:bg-green-600 transition-all duration-300 hover:shadow-xl z-50 flex items-center justify-center"
